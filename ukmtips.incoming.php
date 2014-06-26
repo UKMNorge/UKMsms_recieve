a5335bdc8b283662a689c98a4285e1793d52dc96
Marius Mandal <mariusmandal@gmail.com>
2014-06-26 14:02:23 +0200

    ny sms-klasse

--- ukmtips.incoming.php
+++ ukmtips.incoming.php
@@ -1,20 +1,12 @@
 <?php
-require_once('../send.php');
 
-svevesms_sendSMS(
-    'ukm',
-    $number . ' har sendt dette tipset:' . $melding,
-    90069626,
-    'UKMNorge',
-    0,
-    'ukmtips'
-);
-
-svevesms_sendSMS(
-    'ukm',
-    'Takk for ditt tips!',
-    $number,
-    'UKMNorge',
-    0,
-    'ukmtips2'
-);
+$SMS = new SMS('UkmTips','false');
+$SMS->text($number . ' har sendt dette tipset:' . $melding)
+	->to(90069626)
+	->from('UKMNorge')
+	->ok();
+		
+$SMS->text('Takk for ditt tips!')
+	->to($NUMBER)
+	->from('UKMNorge')
+	->ok();
